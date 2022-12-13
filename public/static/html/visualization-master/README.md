73074e8b941005985332154b9d2ede839f5921d8
koi2000 <1754520660@qq.com>
2022-12-13 20:54:17 +0800

    通过iframe接入原生html

---
+++ public/static/html/visualization-master/README.md
@@ -0,0 +1,28 @@
+# visualization
+字体文件太大传不上去，放群里
+以下是文件树
+```
+visualization
+├─ css
+│  └─ theme.css
+├─ echarts.min.js
+├─ font
+│  └─ 康熙字典体完整版本.TTF
+├─ readme.md
+├─ sample.html
+├─ theme
+│  └─ final-project-theme-echart.js
+└─ themeriver.html
+```
+要求：css需要引入我的theme.js，布局可以自行修改，但是背景、字体（不包括颜色，fontsize）应该和我的保持一致
+
+另外我在https://rgyakazkq7.feishu.cn/docx/FXVRdscCzol4T5xwGMMc1ATUnvh 文档更新了css极速教程，建议看看
+
+sample.html是我用来做实验的，方便查看布局
+
+美术风格可以参考themeriver.html
+
+虽然我的代码烂，但是我写成这个效果还是画了点时间，有写布局设置可以抄我的，比如width设成100%，然后加上resize函数这样缩放chart大小也会跟着改变
+可以看看echart的教程，写的还挺适合新手的
+
+https://echarts.apache.org/handbook/zh/get-started/
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -3,59 +3,98 @@
     <!--    <iframe src="../assets/html/aaaa.html" id="qt"></iframe>-->
     <full-page :options="options" ref="fullpage">
       <div class="section" :key="0">
-        <ChinaMap/>
-      </div>
-      <div class="section">
-        <div>
-          <SentAnalysisSankey v-if="this.index===1" :key="1"/>
-        </div>
-        <!--                        <SentAnalysisSankey/>-->
-      </div>
-
-      <div class="section" :key="2">
-        <div>
-          <SentAnalysisPie v-if="this.index===2"/>
-        </div>
-      </div>
-
-      <div class="section" :key="3">
-<!--        <iframe src="https://www.koi2000.top/"></iframe>-->
-        <!--        <iframe src="../../public/static/html/aaaa.html" width="100%" height="600px"></iframe>-->
-        <iframe src="static/html/visual/sun.html" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>
-      </div>
-
-      <div class="section">
         <div class="slide">
-          <div style="height: 100vh">
-            <img width="100%"
-                 src="https://cdn.cnbj1.fds.api.mi-img.com/product-images/redminote9pro/section14-8.jpg"
-                 alt="">
-          </div>
+          <ChinaMap/>
         </div>
+
         <div class="slide">
-          <div style="height: 100vh">
-            <img width="100%"
-                 src="https://cdn.cnbj1.fds.api.mi-img.com/product-images/redminote9pro/section14-6.jpg"
-                 alt="">
+          <div>
+            <SentAnalysisSankey v-if="this.index===1"/>
           </div>
         </div>
+
+
         <div class="slide">
-          <div style="height: 100vh">
-            <img width="100%"
-                 src="https://cdn.cnbj1.fds.api.mi-img.com/product-images/redminote9pro/section16-2.jpg"
-                 alt="">
+          <div>
+            <SentAnalysisPie v-if="this.index===2"/>
           </div>
         </div>
-      </div>
-      <div class="section">
-        <div class="box3">
-          <div style="height: 100vh">
-            <img height="100%"
-                 src="https://cdn.cnbj1.fds.api.mi-img.com/product-images/redminote9pro/section11-1.jpg"
-                 alt="">
-          </div>
+
+
+        <div class="slide">
+          <iframe src="static/html/visual/sun.html" width="100%" height="100%" frameborder="0"
+                  scrolling="auto"></iframe>
         </div>
+
+        <div class="slide">
+          <Emotion/>
+        </div>
+
+        <div class="slide">
+          <iframe src="static/html/visualization-master/cn.html" width="100%" height="100%" frameborder="0"
+                  scrolling="auto"></iframe>
+        </div>
+
+        <div class="slide">
+          <iframe src="static/html/visualization-master/ka.html" width="100%" height="100%" frameborder="0"
+                  scrolling="auto"></iframe>
+        </div>
+
       </div>
+<!--      <div class="section" :key="0">-->
+<!--        <ChinaMap/>-->
+<!--      </div>-->
+<!--      <div class="section">-->
+<!--        <div>-->
+<!--          <SentAnalysisSankey v-if="this.index===1" :key="1"/>-->
+<!--        </div>-->
+<!--        &lt;!&ndash;                        <SentAnalysisSankey/>&ndash;&gt;-->
+<!--      </div>-->
+
+<!--      <div class="section" :key="2">-->
+<!--        <div>-->
+<!--          <SentAnalysisPie v-if="this.index===2"/>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--      <div class="section" :key="3">-->
+<!--        &lt;!&ndash;        <iframe src="https://www.koi2000.top/"></iframe>&ndash;&gt;-->
+<!--        &lt;!&ndash;        <iframe src="../../public/static/html/aaaa.html" width="100%" height="600px"></iframe>&ndash;&gt;-->
+<!--        <iframe src="static/html/visual/sun.html" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>-->
+<!--      </div>-->
+
+<!--      <div class="section">-->
+<!--        <div class="slide">-->
+<!--          <div style="height: 100vh">-->
+<!--            <img width="100%"-->
+<!--                 src="https://cdn.cnbj1.fds.api.mi-img.com/product-images/redminote9pro/section14-8.jpg"-->
+<!--                 alt="">-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="slide">-->
+<!--          <div style="height: 100vh">-->
+<!--            <img width="100%"-->
+<!--                 src="https://cdn.cnbj1.fds.api.mi-img.com/product-images/redminote9pro/section14-6.jpg"-->
+<!--                 alt="">-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="slide">-->
+<!--          <div style="height: 100vh">-->
+<!--            <img width="100%"-->
+<!--                 src="https://cdn.cnbj1.fds.api.mi-img.com/product-images/redminote9pro/section16-2.jpg"-->
+<!--                 alt="">-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="section">-->
+<!--        <div class="box3">-->
+<!--          <div style="height: 100vh">-->
+<!--            <img height="100%"-->
+<!--                 src="https://cdn.cnbj1.fds.api.mi-img.com/product-images/redminote9pro/section11-1.jpg"-->
+<!--                 alt="">-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
     </full-page>
     <div>
     </div>
@@ -66,9 +105,10 @@ import ChinaMap from "@/views/subviews/ChinaMap";
 import EmotionPlace from "@/views/subviews/EmotionPlace";
 import SentAnalysisSankey from "@/views/subviews/SentAnalysisSankey";
 import SentAnalysisPie from "@/views/subviews/SentAnalysisPie";
+import Emotion from "@/views/subviews/Emotion";
 
 export default {
-  components: {ChinaMap, EmotionPlace, SentAnalysisSankey,SentAnalysisPie},
+  components: {Emotion, ChinaMap, EmotionPlace, SentAnalysisSankey, SentAnalysisPie},
   data() {
     return {
       src: "../../public/static/html/aaaa.html",
@@ -79,6 +119,7 @@ export default {
         navigation: true,
         afterLoad: this.afterLoad,
         onLeave: this.onLeave,
+        afterSlideLoad: this.afterSlideLoad,
         //为每个section设置背景色
         // sectionsColor: [
         //   "#41b883",
@@ -106,6 +147,10 @@ export default {
     onLeave(index, aindex, direction) {
       console.log(aindex.index)
       this.index = aindex.index
+    },
+    afterSlideLoad(anchorLink, index, slideIndex, direction, nextSlideIndex) {
+      console.log(slideIndex.index)
+      this.index = slideIndex.index
     }
   }
 }
